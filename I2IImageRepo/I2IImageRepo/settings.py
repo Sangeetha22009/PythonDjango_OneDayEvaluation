50053b9f1d1058747c14301f36c089bc2484f75f
Sangeetha Ramasamy <sangeetha.ramasamy@ideas2it.com>
2023-05-05 23:05:41 +0530

    Base css commit

--- I2IImageRepo/I2IImageRepo/settings.py
+++ I2IImageRepo/I2IImageRepo/settings.py
@@ -122,14 +122,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS =[
     BASE_DIR / 'static'
 ]
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
