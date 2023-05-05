68cdb7020af860bee4010ec688ba067965f4bebc
Sangeetha Ramasamy <sangeetha.ramasamy@ideas2it.com>
2023-05-05 17:38:30 +0530

    Addition of model and base style

--- I2IImageRepo/ImagesApp/models.py
+++ I2IImageRepo/ImagesApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Gallery(models.Model):
+    title = models.CharField(max_length=50, blank=False, null=False)
+    image_field = models.ImageField(upload_to="uploads/images",blank=False, null=False)
+    description = models.CharField(max_length=200,blank=False, null=False)
+    category = models.CharField(max_length=50, blank=False, null=False)
+    
--- I2IImageRepo/ImagesApp/views.py
+++ I2IImageRepo/ImagesApp/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "ImagesApp/index.html" )
+
+def register(request):
+    return render(request, "ImagesApp/register.html" )
     
