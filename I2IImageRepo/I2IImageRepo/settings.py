ade3c75b0ee1f2ec11a20a6f7167168551aaa23b
Sangeetha Ramasamy <sangeetha.ramasamy@ideas2it.com>
2023-05-06 12:03:25 +0530

    Login_Out functionality commit

--- I2IImageRepo/I2IImageRepo/settings.py
+++ I2IImageRepo/I2IImageRepo/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ImageHub',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'sanram',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / STATIC_URL
+# STATIC_ROOT = BASE_DIR / STATIC_URL
 STATICFILES_DIRS = [
-    '/static'
+   BASE_DIR / 'static'
 ]
 
 # Default primary key field type
--- I2IImageRepo/ImagesApp/views.py
+++ I2IImageRepo/ImagesApp/views.py
@@ -1,24 +1,52 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import ImageUploadForm
+from .forms import ImageUploadForm, UserRegistrationForm
 from .models import Gallery
-
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+
 def index(request):
-    return render(request, "ImagesApp/index.html" )
+    return render(request, "ImagesApp/index.html")
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        if request.method == 'POST':
+            name = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=name, password=password)
+            if user is not None:
+                login(request, user)                
+                return redirect('/')
+            else:
+                messages.error(request, "Inavlid User Name or Password")
+        return render(request, "ImagesApp/login.html")
+
+def user_logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('/')
 
 def register(request):
-    return render(request, "ImagesApp/register.html" )
+    registraion_form = UserRegistrationForm()
+    if request.method == 'POST':
+        registraion_form = UserRegistrationForm(request.POST)
+        if registraion_form.is_valid():
+            registraion_form.save()
+            messages.success(request, 'Registered Successfuly, Please Login..')
+            return redirect('/login')
+    return render(request, "ImagesApp/register.html", {'registraion_form': registraion_form})
 
 def gallery(request):
     images = Gallery.objects.all()
     context = {
-        'images' : images
+        'images': images
     }
     return render(request, 'ImagesApp/gallery.html', context)
 
-
-#image-upload
+# image-upload
 def image_upload(request):
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
@@ -29,18 +57,20 @@ def image_upload(request):
             category = form.cleaned_data['category']
             image = form.cleaned_data['image']
 
-            #save image to server
-            image_obj = Gallery(title=title, description=description, image=image, category=category,)
+            # save image to server
+            image_obj = Gallery(
+                title=title, description=description, image=image, category=category,)
             image_obj.save()
             return redirect('gallery')
-            return HttpResponse('Saved successfully') # redirect to success html or gallery page
+            # redirect to success html or gallery page
+            return HttpResponse('Saved successfully')
         else:
             raise Exception('Error occured ', form.errors)
-            return render(request, 'ImagesApp/image-upload.html')     # need to add messages to display errors
+            # need to add messages to display errors
+            return render(request, 'ImagesApp/image-upload.html')
     else:
         form = ImageUploadForm()
         context = {
             'form', form
         }
         return render(request, 'ImagesApp/image-upload.html')
-    
