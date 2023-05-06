65a09afa6372d556dc955312ad5359a5c1685b54
Subbu <subramanian.palaniappan@ideas2it.com>
2023-05-07 00:12:51 +0530

    todo list and items save and grid changes

--- ToDoApp/ToDoApp/ToDoList/views.py
+++ ToDoApp/ToDoApp/ToDoList/views.py
@@ -6,6 +6,8 @@ from .models import ToDoList, ToDoItem
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 
+from django.db.models import Q
+from django.core.paginator import Paginator
 # Create your views here.
 @login_required(login_url='login')
 def todo_item(request, todolist_id):
@@ -15,14 +17,16 @@ def todo_item(request, todolist_id):
         if form.is_valid():
             title = form.cleaned_data['title']
             description = form.cleaned_data['description']
-            todolist = ToDoList.objects.get_or_create(id=todolist_id)
+            todolist = ToDoList.objects.get(id=todolist_id)
             due_date = form.cleaned_data['due_date']
-            # created_at = form.cleaned_data['created_at']
             is_completed = form.cleaned_data['is_completed']
-            item = ToDoItem(title = title, description=description, due_date= due_date,  is_completed = is_completed)
+            item = ToDoItem( todo_list = todolist, title = title, description=description, due_date= due_date,  is_completed = is_completed)
             item.save()
             messages.success(request, 'Item added succesfully !')
-            items = ToDoItem.objects.all()
+            print(item)
+            print(todolist_id)
+            items = ToDoItem.objects.all().order_by('-id').filter(todo_list__id = todolist_id)
+            print(items)
             context = {
                 'items' : items,
                 'todolist_id': todolist_id
@@ -32,14 +36,15 @@ def todo_item(request, todolist_id):
             messages.error(request, 'Invalid form, please check and update')
         return render(request, "ToDoList/todo-items.html")
     else:
-        items = ToDoItem.objects.all().order_by('-id')
-        paginator = Paginator(items, 5)
-        current_page = request.GET.get('page')
-        paged_items = paginator.get_page(current_page)
+        print(todolist_id)
+        items = ToDoItem.objects.all().order_by('-id').filter(todo_list__id = todolist_id)
+        # paginator = Paginator(items, 5)
+        # current_page = request.GET.get('page')
+        # paged_items = paginator.get_page(current_page)
         form = ToDoItemForm()
         context = {
             'form': form,
-            'items': paged_items,
+            'items': items,
             'todolist_id': todolist_id
         } 
         return render(request, "ToDoList/todo-items.html", context)
@@ -82,11 +87,27 @@ def todo_list(request):
             if form.is_valid():
                 title = form.cleaned_data['title']
                 description = form.cleaned_data['description']
-
                 list_obj = ToDoList(
-                    title=title, description=description, uploaded_by = request.user.username )
+                    title=title, description=description, created_by = request.user )
                 list_obj.save()
                 messages.success(request, 'List Created Successfully !!')
-                return redirect('/')
-    return render(request, "ToDoList/todo-list.html")
+                
+                listobj = ToDoList.objects.all().order_by('-id').filter(Q(created_by = request.user))
+                count = listobj.count
+                context = {
+                    'listobj' : listobj,
+                    'count': count
+                }
+                return render(request, 'ToDoList/todo-list.html', context)
+            else:
+                messages.error(request, 'Error occured while saving')
+        else:
+            listobj = ToDoList.objects.all().order_by('-id') .filter(Q(created_by = request.user))
+            count = listobj.count
+            context = {
+                'listobj' : listobj,
+                'count': count
+            }
+            return render(request, 'ToDoList/todo-list.html', context)
+    return redirect('login')
 
