a5fe45ac6a536bced89f34ab5e0dffb1ab290896
Sangeetha Ramasamy <sangeetha.ramasamy@ideas2it.com>
2023-05-07 10:31:43 +0530

    Initial commit of blog app

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogapp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BlogApp',
+        'USER': 'postgres',
+        'PASSWORD': 'sanram',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
@@ -116,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
