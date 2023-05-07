fcf6b0555981a85ace9447a8a95f7d2e3a04397a
Subbu <subramanian.palaniappan@ideas2it.com>
2023-05-07 15:23:16 +0530

    To Do List Edit, Delete missing changes

--- ToDoApp/ToDoApp/ToDoList/urls.py
+++ ToDoApp/ToDoApp/ToDoList/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("logout", views.user_logout, name = "logout" ),
     path("todo-item/<int:todolist_id>/", views.todo_item, name='todo-item'),
     path("todo-list", views.todo_list, name = "todo-list" ),
-    path("delete-item/<int:todo_item_id>/<int:todo_list_id>/", views.delete_item, name="delete-item"),
-    path("edit-item/<int:todo_item_id>/<int:todo_list_id>/", views.delete_item, name="edit-item")
+    path("delete-item/", views.delete_item, name="delete-item"),
+    path("edit-item/<int:todo_item_id>/<int:todo_list_id>/", views.delete_item, name="edit-item"),
+    path('delete-list/<int:list_id>/', views.delete_list, name="delete-list"),
+    path('edit-list/<int:list_id>/', views.edit_list, name="edit-list")
 ]
--- ToDoApp/ToDoApp/ToDoList/views.py
+++ ToDoApp/ToDoApp/ToDoList/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -6,36 +7,31 @@ from .models import ToDoList, ToDoItem
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.core.paginator import Paginator
+import json
 
 # Create your views here.
 
 @login_required(login_url='login')
-def edit_item(request, item_id, todo_list_id):
+def edit_item(request, item_id):
+    breakpoint()
+    item = ToDoItem.objects.get(id=item_id)
+    todo_list_id = item.todo_list.id
     return redirect('todo-item/' + str(todo_list_id))
 
 @login_required(login_url='login')
-def delete_item(request, todo_item_id, todo_list_id):
-    # prompt = input('are you sure want to delete?')
-    # if prompt == 'yes':
-        item = ToDoItem.objects.filter(id=todo_item_id)
-        if item is not None:
-            item.delete()
-            items = ToDoItem.objects.all().order_by('-id').filter(todo_list__id = todo_list_id)
-            form = ToDoItemForm()
-            context = {
-                'form': form,
-                'items': items,
-                'todolist_id': todo_list_id
-            } 
-            return render(request, "ToDoList/todo-items.html", context)
-        else:
-            messages.error(request, 'Item not found')
-            return redirect('/todo-item/' + str(todo_list_id))
-    # else:
-    #     return redirect('/todo-item/' + str(todo_list_id))
-
-        
-
+def delete_item(request):
+    body = json.loads(request.body)
+    todo_item_id = body['todo_item_id']
+    item = ToDoItem.objects.filter(id=todo_item_id)                
+    if item is not None:
+        item.delete()
+        return JsonResponse({
+            'is_deleted' : True
+        })
+    else:
+        messages.error(request, 'Item not found')
+        return render(request, "ToDoList/todo-items.html")
+     
 @login_required(login_url='login')
 def todo_item(request, todolist_id):
     if request.method == 'POST':
@@ -60,9 +56,6 @@ def todo_item(request, todolist_id):
         return render(request, "ToDoList/todo-items.html")
     else:
         items = ToDoItem.objects.all().order_by('-id').filter(todo_list__id = todolist_id)
-        # paginator = Paginator(items, 5)
-        # current_page = request.GET.get('page')
-        # paged_items = paginator.get_page(current_page)
         form = ToDoItemForm()
         context = {
             'form': form,
@@ -105,14 +98,22 @@ def register(request):
 def todo_list(request):
     if request.user.is_authenticated:
         if request.method == 'POST':
-            form = ToDoListForm(request.POST)
+            form = ToDoListForm(request.POST, auto_id=True)
             if form.is_valid():
                 title = form.cleaned_data['title']
                 description = form.cleaned_data['description']
-                list_obj = ToDoList(
-                    title=title, description=description, created_by = request.user )
-                list_obj.save()
-                messages.success(request, 'List Created Successfully !!')
+                id = request.POST.get('id')
+                if id is not None:
+                    list_obj = ToDoList.objects.get(id=id)
+                    list_obj.title = title
+                    list_obj.description = description
+                    list_obj.save()
+                    messages.success(request, 'List Updated Successfully !!')
+                else: 
+                    list_obj = ToDoList(
+                        title=title, description=description, created_by = request.user )
+                    list_obj.save()
+                    messages.success(request, 'List Created Successfully !!')
                 
                 listobj = ToDoList.objects.all().order_by('-id').filter(Q(created_by = request.user))
                 count = listobj.count
@@ -132,4 +133,35 @@ def todo_list(request):
             }
             return render(request, 'ToDoList/todo-list.html', context)
     return redirect('login')
+
+@login_required(login_url='login')
+def delete_list(request, list_id):
+    list = ToDoList.objects.filter(id=list_id)                
+    if list is not None:
+        list.delete()
+        listobj = ToDoList.objects.all().order_by('-id') .filter(Q(created_by = request.user))
+        count = listobj.count
+        context = {
+            'listobj' : listobj,
+            'count': count
+        }
+        messages.success(request, 'Todo list record deleted successfully !')
+        return render(request, 'ToDoList/todo-list.html', context)
+    else:
+        messages.error(request, 'Item not found')
+        return render(request, "ToDoList/todo-items.html")
+    
+
+@login_required(login_url='login')
+def edit_list(request, list_id):
+    list = ToDoList.objects.get(id=list_id)                
+    listobj = ToDoList.objects.all().order_by('-id') .filter(Q(created_by = request.user))
+    count = listobj.count
+    context = {
+        'listobj' : listobj,
+        'count': count,
+        'item': list,
+        'is_edit':True
+    }
+    return render(request, 'ToDoList/todo-list.html', context)
 
