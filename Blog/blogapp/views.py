428bd8300e5e4302806682654c43200d6676a33c
Sangeetha Ramasamy <sangeetha.ramasamy@ideas2it.com>
2023-05-08 23:44:45 +0530

    Code optimization for blog views.py file commit

--- Blog/blogapp/views.py
+++ Blog/blogapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import UserRegistrationForm, BlogForm, BlogPostForm
 from .models import Blog, BlogPost, Comment,  Share
 from django.contrib.auth.decorators import login_required
@@ -12,185 +13,195 @@ from django.urls import reverse
 
 @login_required(login_url="login")
 def index(request):
-    keyword = request.GET.get('keyword')    
-    if keyword is None:
-        keyword = ''
-    blogs = Blog.objects.filter(Q(created_by=request.user.id) & (Q(title__icontains=keyword)
-                                | Q(description__icontains=keyword))).order_by('-id')
+    search_keyword = request.GET.get('keyword', '')
+    blogs = Blog.objects.filter(Q(created_by=request.user.id) & (Q(title__icontains=search_keyword)
+                                | Q(description__icontains=search_keyword))).order_by('-id')
     paginator = Paginator(blogs, 5)
-    current_page = request.GET.get('page')
-    if current_page is None:
-        current_page = 1
+    current_page = request.GET.get('page') or 1
     paged_blogs = paginator.get_page(current_page)
-    return render(request, "blogapp/index.html", {'blogs':paged_blogs, 'count': blogs.count })
+    return render(request, "blogapp/index.html", {'blogs': paged_blogs, 'count': blogs.count()})
 
+
+@login_required(login_url="login")
 def user_login(request):
     if request.user.is_authenticated:
         return redirect('/')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, "Invalid username or password.")
     else:
-        if request.method == 'POST':
-            name = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=name, password=password)
-            if user is not None:
-                login(request, user)                
-                return redirect('/')
-            else:
-                messages.error(request, "Inavlid User Name or Password")
-        return render(request, "blogapp/login.html")
+        form = AuthenticationForm()
+
+    return render(request, "blogapp/login.html", {'form': form})
+
 
 def user_logout(request):
     if request.user.is_authenticated:
         logout(request)
+        messages.success(request, "You have been successfully logged out.")
     return redirect('/')
 
+
 def register(request):
-    registraion_form = UserRegistrationForm()
+    registration_form = UserRegistrationForm()
     if request.method == 'POST':
-        registraion_form = UserRegistrationForm(request.POST)
-        if registraion_form.is_valid():
-            registraion_form.save()
+        registration_form = UserRegistrationForm(request.POST)
+        if registration_form.is_valid():
+            registration_form.save()
             messages.success(request, 'Registered Successfuly, Please Login..')
             return redirect('/login')
-    return render(request, "blogapp/register.html", {'registraion_form': registraion_form})
+    return render(request, "blogapp/register.html", {'registraion_form': registration_form})
 
 
 @login_required(login_url='login')
-def create_blog(request):    
-        if request.method == 'POST':
-            form = BlogForm(request.POST, request.FILES)
-            if form.is_valid():
-                title = form.cleaned_data['title']
-                description = form.cleaned_data['description']
-                image = form.cleaned_data['cover_image']
-                blog = Blog(
-                    title=title, description=description, cover_image = image, created_by = request.user )                
-                blog.save()
-                messages.success(request, 'Blog created successfully!!')                
-                return redirect('/')
-            else:
-                messages.error(request, 'Error occured while creating blog!!')
-                return render(request, 'blogapp/create-blog.html') 
-        else:            
-            return render(request, 'blogapp/create-blog.html')    
+def create_blog(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            description = form.cleaned_data['description']
+            image = form.cleaned_data['cover_image']
+            blog = Blog(
+                title=title, description=description, cover_image=image, created_by=request.user)
+            blog.save()
+            messages.success(request, 'Blog created successfully!!')
+            return redirect('/')
+        else:
+            messages.error(request, 'Error occured while creating blog!!')
+            return render(request, 'blogapp/create-blog.html')
+    form = BlogForm()
+    return render(request, 'blogapp/create-blog.html')
 
 
 @login_required(login_url='login')
 def view_posts(request, blog_id):
-    keyword = request.GET.get('keyword')  
-    if keyword is None:
-        keyword = ''
-    posts = BlogPost.objects.filter(Q(blog__id=blog_id) & Q(created_by=request.user.id) & (Q(title__icontains=keyword)
-                                | Q(content__icontains=keyword))).order_by('-id')
+    post_search_keyword = request.GET.get('keyword', '')
+    posts = BlogPost.objects.select_related('blog').filter(Q(blog__id=blog_id) & Q(created_by=request.user.id) & (Q(title__icontains=post_search_keyword)
+                                                                                                   | Q(content__icontains=post_search_keyword))).order_by('-id')
     paginator = Paginator(posts, 5)
-    current_page = request.GET.get('page')
-    if current_page is None:
-        current_page = 1
+    current_page = request.GET.get('page', 1)
     paged_posts = paginator.get_page(current_page)
-    return render(request, 'blogapp/view-posts.html', {'blog_id': blog_id, 'posts':paged_posts,  'count': posts.count })
+    return render(request, 'blogapp/view-posts.html', {'blog_id': blog_id, 'posts': paged_posts,  'count': posts.count})
 
 
 @login_required(login_url='login')
-def add_edit_post(request, blog_id, post_id = None):
+def add_edit_post(request, blog_id, post_id=None):
     if request.method == 'POST':
-            form = BlogPostForm(request.POST, request.FILES)
-            if form.is_valid():
-                title = form.cleaned_data['title']
-                content = form.cleaned_data['content']
-                image = form.cleaned_data['image']
-                if post_id is None:
-                    post_id = request.POST.get('post_id')
-                blog = Blog.objects.get(id=blog_id)
-                if post_id is not None:
-                    blog_post = BlogPost.objects.get(id=post_id)
-                    blog_post.blog = blog
-                    blog_post.title = title
-                    blog_post.content = content
-                    blog_post.save()
-                    messages.success(request, 'Blog Post Updated Successfully !!')
-                else: 
-                    blog_post = BlogPost(
-                    title=title, content=content, image = image, created_by = request.user, blog = blog)                
-                    blog_post.save()
-                    messages.success(request, 'Blog post created successfully!!')  
-                url = reverse('view-posts', args=[str(blog_id)])              
-                return redirect(url)
+        form = BlogPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            image = form.cleaned_data['image']
+            if post_id is None:
+                post_id = request.POST.get('post_id')
+            blog = Blog.objects.get(id=blog_id)
+            if post_id is not None:
+                blog_post = BlogPost.objects.get(id=post_id)
+                blog_post.blog = blog
+                blog_post.title = title
+                blog_post.content = content
+                blog_post.save()
+                messages.success(request, 'Blog Post Updated Successfully !!')
             else:
-                messages.error(request, r'Error occured while creating\updating blog post!!')
-                return render(request, 'blogapp/add-edit-post.html', {'blog_id':blog_id}) 
+                blog_post = BlogPost(
+                    title=title, content=content, image=image, created_by=request.user, blog=blog)
+                blog_post.save()
+                messages.success(request, 'Blog post created successfully!!')
+            url = reverse('view-posts', args=[str(blog_id)])
+            return redirect(url)
+        else:
+            messages.error(
+                request, r'Error occured while creating\updating blog post!!')
+            return render(request, 'blogapp/add-edit-post.html', {'blog_id': blog_id})
     else:
         if post_id is not None:
-            post = BlogPost.objects.get(id=post_id)                
-            postobj = BlogPost.objects.all().order_by('-id') .filter(Q(created_by = request.user))
+            post = BlogPost.objects.get(id=post_id)
+            postobj = BlogPost.objects.all().order_by(
+                '-id') .filter(Q(created_by=request.user))
             count = postobj.count
             context = {
-                # 'postobj' : postobj,
                 'count': count,
                 'item': post,
-                'is_edit':True,
+                'is_edit': True,
                 'blog_id': blog_id
             }
             return render(request, 'blogapp/add-edit-post.html', context)
-        else:            
-            return render(request, 'blogapp/add-edit-post.html', {'blog_id':blog_id} )
+        else:
+            return render(request, 'blogapp/add-edit-post.html', {'blog_id': blog_id})
 
-
-@login_required(login_url = 'login')    
-def post_comments(request, post_id):
-    post = BlogPost.objects.get(id=post_id)    
-    if request.method=="POST":
-        comment = request.POST.get('comment')
-        comment = Comment(comment = comment, post=post, created_by = request.user)
-        comment.save()
-    comments = Comment.objects.filter(post=post)
-    return render(request, "blogapp/post-comments.html", {'post':post, 'comments':comments}) 
 
 @login_required(login_url='login')
-def delete_post(request,post_id):
-    post = BlogPost.objects.get(id=post_id)
-    blog_id = post.blog.id    
-    if post is not None:
+def post_comments(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    comments = Comment.objects.filter(post=post).select_related('created_by')
+
+    if request.method == "POST":
+        comment = request.POST.get('comment')
+        comment = Comment(comment=comment, post=post, created_by=request.user)
+        comment.save()
+
+    return render(request, "blogapp/post-comments.html", {'post': post, 'comments': comments})
+
+
+@login_required(login_url='login')
+def delete_post(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    blog_id = post.blog.id
+
+    if post:
         post.delete()
         messages.success(request, 'Post deleted successfully !')
-    url = reverse('view-posts', args=[str(blog_id)])              
-    return redirect(url)
-
-@login_required(login_url='login')
-def like_post(request,post_id,is_like):
-    post = BlogPost.objects.get(id=post_id)
-    blog_id = post.blog.id
-    if is_like is not None:
-        is_like = bool(is_like)
-        if post is not None:
-            post.is_like = is_like
-            post.save()
-            if is_like == True:
-                messages.success(request, 'Post liked successfully !')
-                
-            else:
-                messages.success(request, 'Post dis-liked successfully !')
-
-    url = reverse('view-posts', args=[str(blog_id)])              
+    url = reverse('view-posts', args=[str(blog_id)])
     return redirect(url)
 
 
 @login_required(login_url='login')
-def share_post(request,post_id,shared_to):
-    post = BlogPost.objects.get(id=post_id)
+def like_post(request, post_id, is_like):
+    post = get_object_or_404(BlogPost, id=post_id)
     blog_id = post.blog.id
-    if shared_to is not None:
-        if post is not None:
-            share = Share(post = post, social_media = shared_to, created_by = request.user)
-            share.save()
-            messages.success(request, 'Post shared to ' + shared_to + ' successfully !')
-    url = reverse('view-posts', args=[str(blog_id)])              
+    if post:
+        post.is_like = is_like
+        post.save()
+
+        if is_like:
+            messages.success(request, 'Post liked successfully!')
+        else:
+            messages.success(request, 'Post disliked successfully!')
+
+    url = reverse('view-posts', args=[str(blog_id)])
     return redirect(url)
 
+
+@login_required(login_url='login')
+def share_post(request, post_id, shared_to):
+    post = get_object_or_404(BlogPost, id=post_id)
+    blog_id = post.blog.id
+
+    allowed_social_media = ['Facebook', 'Twitter']
+
+    if shared_to in allowed_social_media and post:
+        share = Share(post=post, social_media=shared_to,
+                      created_by=request.user)
+        share.save()
+        messages.success(request, f'Post shared to {shared_to} successfully!')
+
+    url = reverse('view-posts', args=[str(blog_id)])
+    return redirect(url)
+
+
+@login_required(login_url='login')
 def read_more_blog(request, blog_id):
-    blog = Blog.objects.get(id=blog_id)
-    posts = BlogPost.objects.all().filter(blog_id = blog_id)        
+    blog = get_object_or_404(Blog, id=blog_id)
+    posts = blog.blogpost_set.all()
+    BlogPost.objects.all().filter(blog_id = blog_id)
     context = {
-        'posts' : posts,
+        'posts': posts,
         'blog': blog,
     }
-    return render(request, 'blogapp/read-more-blog.html' , context)
+    return render(request, 'blogapp/read-more-blog.html', context)
